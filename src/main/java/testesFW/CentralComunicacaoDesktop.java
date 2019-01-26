3a7fe0aa481d9deb6f7923e24384da89600e164a
Salvio Furbino Elias <salviof@gmail.com>
2019-01-26 00:15:40 -0200

    Atualizacao automática

--- src/main/java/testesFW/CentralComunicacaoDesktop.java
+++ src/main/java/testesFW/CentralComunicacaoDesktop.java
@@ -18,7 +18,7 @@ import com.super_bits.modulosSB.SBCore.modulos.comunicacao.ItfDisparoComunicacao
 import com.super_bits.modulosSB.SBCore.modulos.comunicacao.ItfModeloMensagem;
 import com.super_bits.modulosSB.SBCore.modulos.comunicacao.ItfTipoTransporteComunicacao;
 import org.coletivojava.fw.api.tratamentoErros.FabErro;
-import com.super_bits.modulosSB.SBCore.modulos.objetos.InfoCampos.ItensGenericos.basico.UsuarioOSistema;
+import com.super_bits.modulosSB.SBCore.modulos.objetos.InfoCampos.ItensGenericos.basico.UsuarioAplicacaoEmExecucao;
 import com.super_bits.modulosSB.SBCore.modulos.objetos.registro.Interfaces.basico.ItfUsuario;
 import javax.swing.JOptionPane;
 import com.super_bits.modulosSB.SBCore.modulos.comunicacao.ItffabricaTrasporteComunicacao;
@@ -99,7 +99,7 @@ public class CentralComunicacaoDesktop extends CentralComunicaoAbstrato {
     @Override
     public ItfComunicacao gerarComunicacaoSistema_UsuairoLogado(FabTipoComunicacao tipocomunicacao, String mensagem, ItffabricaTrasporteComunicacao... tiposTransporte) {
         ItfComunicacao comunicacao
-                = new ComunicacaoTransient(new UsuarioOSistema(), SBCore.getUsuarioLogado(),
+                = new ComunicacaoTransient(new UsuarioAplicacaoEmExecucao(), SBCore.getUsuarioLogado(),
                         tipocomunicacao.getRegistro(), gerarListaTransportes(tiposTransporte));
         comunicacao.setMensagem(mensagem);
         comunicacao.setNome(mensagem);
