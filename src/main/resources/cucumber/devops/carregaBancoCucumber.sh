42e178c666b22820488071ac6845fba085ad3c42
Salvio Furbino <salviof@gmail.com>
2022-02-06 02:18:51 -0300

    Atualizacao automática

--- src/main/resources/cucumber/devops/carregaBancoCucumber.sh
+++ src/main/resources/cucumber/devops/carregaBancoCucumber.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ./SBProjeto.prop
 SLUG_REQUISITO=$1
-mysqladmin -u root -psenhaDev#123 drop $NOME_BANCO -f
-mysqladmin -u root -psenhaDev#123 create $NOME_BANCO
-mysql -u root $NOME_BANCO -psenhaDev#123 < ./$SLUG_REQUISITO.cucumber.sql
+docker exec  devopsTestes1 -w /devopsDBScript/ mysqladmin -u root -psenhaDev#123 drop $NOME_BANCO -f
+docker exec  devopsTestes1 -w /devopsDBScript/ mysqladmin -u root -psenhaDev#123 create $NOME_BANCO
+docker exec  devopsTestes1 /bin/bash -c 'mysql -u root '$NOME_BANCO' -psenhaDev#123 < /devopsDBScript/'$SLUG_REQUISITO'.cucumber.sql'
--- src/main/resources/cucumber/devops/compilaBancoCucumber.sh
+++ src/main/resources/cucumber/devops/compilaBancoCucumber.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ./SBProjeto.prop
 SLUG_REQUISITO=$1
-mysqldump -alv -u root -psenhaDev#123  $NOME_BANCO > $SLUG_REQUISITO.cucumber.sql
+docker exec devopsTestes1 /bin/bash -c 'mysqldump -alv -u root -psenhaDev#123  '$NOME_BANCO' > /devopsDBScript/'$SLUG_REQUISITO'.cucumber.sql'
 
